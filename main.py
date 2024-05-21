0f8b33c2491d863db3102233939a4ae95f1c8045
unknown <tomchaussi6@gmail.com>
2024-05-21 04:22:38 +0200

    Application du background

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ pygame.display.set_caption("Space_War")
 screen = pygame.display.set_mode((400, 600))
 
 background = pygame.image.load("img/fond.jpeg")
+background.convert()
 
 running = True
 
 while running:
+    screen.blit(background, (0, 0))
     pygame.display.flip()
     
     for event in pygame.event.get():
